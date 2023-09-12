304046effc2bbeaf73dcddc10bd764f40f30fc7a
Eric Qian <ericweiqian@gmail.com>
2023-09-12 08:45:57 -0700

    Completed fibonacci_numbers.rb

--- RB110/lesson-1-ruby-collections/small-problems-medium-1/fibonacci_numbers.rb
+++ RB110/lesson-1-ruby-collections/small-problems-medium-1/fibonacci_numbers.rb
@@ -23,18 +23,32 @@ def fibonacci_procedural(n)
   last
 end
 
-#p fibonacci_procedural(20) == 6765
-#p fibonacci_procedural(100) == 354224848179261915075
-#p fibonacci_procedural(100_001) # => 4202692702.....8285979669707537501
+#p fibonacci_procedural_og(20) == 6765
+#p fibonacci_procedural_og(100) == 354224848179261915075
+#p fibonacci_procedural_og(100_001) # => 4202692702.....8285979669707537501
 
 def fibonacci_last(n)
+  n = (n <= 60 ? n : n.remainder(60))
   fibonacci_procedural(n).to_s[-1].to_i
 end
 
+=begin Print first 100 fib last digits to identify a pattern
+(1..100).each do |num|
+  if num % 10 == 0
+    puts "#{fibonacci_last(num)} "
+  else
+    print "#{fibonacci_last(num)} "
+  end
+end
+=end
+
+#(96..104).each { |n| puts fibonacci_procedural(n) }
+
 p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
 p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(62)
 p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
 p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
 p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
 p fibonacci_last(123456789) # -> 4
-
+p fibonacci_last(123_456_789_987_745) # -> 5
