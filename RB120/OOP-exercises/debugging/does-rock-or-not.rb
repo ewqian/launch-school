295f7ae26338b86e7fd5b333175be6db622e0444
Eric Qian <ericweiqian@gmail.com>
2023-11-30 08:20:31 -0800

    last debugging and medium 1 exercises

--- RB120/OOP-exercises/debugging/does-rock-or-not.rb
+++ RB120/OOP-exercises/debugging/does-rock-or-not.rb
@@ -1,4 +1,6 @@
-class AuthenticationError < Exception; end
+require 'pry'
+
+class AuthenticationError < StandardError; end
 
 # A mock search engine
 # that returns a random number instead of an actual count.
@@ -19,7 +21,7 @@ class SearchEngine
 end
 
 module DoesItRock
-  API_KEY = 'asds' #'LS1A'
+  API_KEY = 'LS1A' #'LS1A'
 
   class NoScore; end
 
@@ -29,8 +31,9 @@ module DoesItRock
       negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY)
 
       (positive * 100) / (positive + negative)
-    rescue Exception
-      NoScore
+      100/0
+    rescue ZeroDivisionError
+      NoScore.new
     end
   end
 
@@ -47,7 +50,7 @@ module DoesItRock
     else
       "#{term} rocks!"
     end
-  rescue Exception => e
+  rescue StandardError => e
     e.message
   end
 end
@@ -57,3 +60,25 @@ end
 puts DoesItRock.find_out('Sushi')       # Sushi seems to be ok...
 puts DoesItRock.find_out('Rain')        # Rain is not fun.
 puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
+
+# Issue 1
+# The AuthenticationError is raised, but the Score::for_term method catches it, so it never reaches
+  # the DoesItRock::find_out method.
+
+# Solution 1
+# have the AuthenticationError inherit from Standard Error which we'll use instead of Exception
+  # since it's more specific and still allows the system to raise more critical issues
+# in the Score method change this to  ZeroDivisionError where we'll let a value to return so that
+  # the case statement within the find_out method can execute
+# for an AuthenticationError catch it in the find_out method
+
+# Issue 2
+# When the return value of Score::for_term is NoScore, the case statement in DoesItRock::find_out
+  # does not behave as expected.
+
+# Solution 2
+# "#{term} rocks!" is outputted everytime because of how case statements are implemented.
+# they use Object#=== which by default determines if the case value is an instance of the class
+  # provided in the when conditional
+# so the statement NoClass === NoClass is false because NoClass is not an instance of itself
+# change the rescue StandardError to NoScore.new to fix this
---
+++ RB120/OOP-exercises/medium-1/poker.rb
@@ -0,0 +1,252 @@
+class PokerHand
+
+  def initialize(deck)
+    @cards = deck.instance_of?(Deck) ? deck.deck[0..4] : deck[0..4]
+  end
+
+  def print
+    puts cards
+  end
+
+  def evaluate
+    case
+    when royal_flush?     then 'Royal flush'
+    when straight_flush?  then 'Straight flush'
+    when four_of_a_kind?  then 'Four of a kind'
+    when full_house?      then 'Full house'
+    when flush?           then 'Flush'
+    when straight?        then 'Straight'
+    when three_of_a_kind? then 'Three of a kind'
+    when two_pair?        then 'Two pair'
+    when pair?            then 'Pair'
+    else                       'High card'
+    end
+  end
+
+  private
+
+  attr_reader :cards
+
+  # A, K, Q, J, 10 all of the same suit
+  def royal_flush?
+    flush?() && (cards.map { |card| card.value }.sort == [10, 11, 12, 13, 14])
+  end
+
+  # Five cards of the same suit in sequence
+  def straight_flush?
+    flush?() && straight?()
+  end
+
+  # Four cards of the same rank
+  def four_of_a_kind?
+    hand_ranks.each { |rank| return true if hand_ranks.count(rank) == 4 }
+
+    false
+  end
+
+  # Three cards of one rank and two of another
+  def full_house?
+    rank_count = hand_ranks.uniq.map { |rank| hand_ranks.count(rank) }.sort
+    rank_count.first == 2 && rank_count.last == 3
+  end
+
+  # Five cards of the same suit
+  def flush?
+    cards.map { |card| card.suit }.uniq.count == 1
+  end
+
+  # Five cards in sequence (for example, 4, 5, 6, 7, 8)
+  def straight?
+    sorted_vals = cards.map { |card| card.value }.sort
+    incr_seq = (sorted_vals.first .. (sorted_vals.first + 4)).to_a
+    decr_seq = (sorted_vals.last).downto(sorted_vals.last - 4).to_a
+
+    sorted_vals == incr_seq || sorted_vals == decr_seq
+  end
+
+  # Three cards of the same rank
+  def three_of_a_kind?
+    hand_ranks.each { |rank| return true if hand_ranks.count(rank) == 3 }
+
+    false
+  end
+
+  # Two cards of one rank and two cards of another
+  def two_pair?
+    uniq_ranks = hand_ranks.uniq
+    num_pairs = uniq_ranks.count { |rank| hand_ranks.count(rank) == 2 }
+
+    num_pairs == 2
+  end
+
+  # Two cards of the same rank
+  def pair?
+    hand_ranks.each { |rank| return true if hand_ranks.count(rank) == 2 }
+
+    false
+  end
+
+  def hand_ranks
+    cards.map { |card| card.rank }
+  end
+end
+
+class Deck
+  RANKS = ((2..10).to_a + %w(Jack Queen King Ace)).freeze
+  SUITS = %w(Hearts Clubs Diamonds Spades).freeze
+
+  attr_reader :deck
+
+  def initialize
+    reset
+  end
+
+  def draw
+    reset if @deck.empty?
+    @deck.pop
+  end
+
+  private
+
+  def reset
+    @deck = RANKS.product(SUITS).map do |rank, suit|
+      Card.new(rank, suit)
+    end
+
+    @deck.shuffle!
+  end
+end
+
+class Card
+  include Comparable
+  attr_reader :rank, :suit
+
+  VALUES = { 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def value
+    VALUES.fetch(rank, rank)
+  end
+
+  def <=>(other_card)
+    value <=> other_card.value
+  end
+end
+
+hand = PokerHand.new(Deck.new)
+hand.print
+puts hand.evaluate
+
+# Danger danger danger: monkey
+# patching for testing purposes.
+class Array
+  alias_method :draw, :pop
+end
+
+# Test that we can identify each PokerHand type.
+hand = PokerHand.new([
+  Card.new(10,      'Hearts'),
+  Card.new('Ace',   'Hearts'),
+  Card.new('Queen', 'Hearts'),
+  Card.new('King',  'Hearts'),
+  Card.new('Jack',  'Hearts')
+])
+puts hand.evaluate == 'Royal flush'
+
+hand = PokerHand.new([
+  Card.new(8,       'Clubs'),
+  Card.new(9,       'Clubs'),
+  Card.new('Queen', 'Clubs'),
+  Card.new(10,      'Clubs'),
+  Card.new('Jack',  'Clubs')
+])
+puts hand.evaluate == 'Straight flush'
+
+hand = PokerHand.new([
+  Card.new(3, 'Hearts'),
+  Card.new(3, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(3, 'Spades'),
+  Card.new(3, 'Diamonds')
+])
+puts hand.evaluate == 'Four of a kind'
+
+hand = PokerHand.new([
+  Card.new(3, 'Hearts'),
+  Card.new(3, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(3, 'Spades'),
+  Card.new(5, 'Hearts')
+])
+puts hand.evaluate == 'Full house'
+
+hand = PokerHand.new([
+  Card.new(10, 'Hearts'),
+  Card.new('Ace', 'Hearts'),
+  Card.new(2, 'Hearts'),
+  Card.new('King', 'Hearts'),
+  Card.new(3, 'Hearts')
+])
+puts hand.evaluate == 'Flush'
+
+hand = PokerHand.new([
+  Card.new(8,      'Clubs'),
+  Card.new(9,      'Diamonds'),
+  Card.new(10,     'Clubs'),
+  Card.new(7,      'Hearts'),
+  Card.new('Jack', 'Clubs')
+])
+puts hand.evaluate == 'Straight'
+
+hand = PokerHand.new([
+  Card.new('Queen', 'Clubs'),
+  Card.new('King',  'Diamonds'),
+  Card.new(10,      'Clubs'),
+  Card.new('Ace',   'Hearts'),
+  Card.new('Jack',  'Clubs')
+])
+puts hand.evaluate == 'Straight'
+
+hand = PokerHand.new([
+  Card.new(3, 'Hearts'),
+  Card.new(3, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(3, 'Spades'),
+  Card.new(6, 'Diamonds')
+])
+puts hand.evaluate == 'Three of a kind'
+
+hand = PokerHand.new([
+  Card.new(9, 'Hearts'),
+  Card.new(9, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(8, 'Spades'),
+  Card.new(5, 'Hearts')
+])
+puts hand.evaluate == 'Two pair'
+
+hand = PokerHand.new([
+  Card.new(2, 'Hearts'),
+  Card.new(9, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(9, 'Spades'),
+  Card.new(3, 'Diamonds')
+])
+puts hand.evaluate == 'Pair'
+
+hand = PokerHand.new([
+  Card.new(2,      'Hearts'),
+  Card.new('King', 'Clubs'),
+  Card.new(5,      'Diamonds'),
+  Card.new(9,      'Spades'),
+  Card.new(3,      'Diamonds')
+])
+puts hand.evaluate == 'High card'
